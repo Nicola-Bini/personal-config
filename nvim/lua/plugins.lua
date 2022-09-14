29c5cf1f2643e1839e2477ae1c7064cb8b91c08a
Nicola Bini <63654846+Nicola-Bini@users.noreply.github.com>
2022-09-14 13:22:50 -0400

    commmi local canghe to jjjjjjjjhhhhhhhupdate

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,34 @@
+packer = require('packer')
+
+packer.init({
+    opt_default = false, -- Default to using opt (as opposed to start) plugins)
+    display = {
+      open_fn  = require('packer.util').float, -- An optional function to open a window for packer's display
+      working_sym = '⟳', -- The symbol for a plugin being installed/updated
+      error_sym = '✗', -- The symbol for a plugin with an error in installation/updating
+      done_sym = '✓', -- The symbol for a plugin which has completed installation/updating
+      removed_sym = '-', -- The symbol for an unused plugin which was removed
+      moved_sym = '→', -- The symbol for a plugin which was moved (e.g. from opt to start)
+      header_sym = '━', -- The symbol for the header line in packer's display
+      show_all_info = true, -- Should packer show all update details automatically?
+      prompt_border = 'double', -- Border style of prompt popups.
+  }
+})
+
+local use = packer.use
+packer.reset()
+
+packer.startup(function()
+  use {
+    'wbthomason/packer.nvim',
+    opt = false
+
+  }
+
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  -- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+  } 
+  
+end)
