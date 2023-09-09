3114a140132b942cc4fa199ae922556bfed814f7
Nicola-Bini <bini.nicola6@gmail.com>
2023-09-09 11:23:22 -0400

    Move plugin "lspconfig" in plugins "folder" and add "vim" as global

--- nvim/lua/nb/lsp.lua
+++
@@ -1,5 +0,0 @@
-local lspconfig = require'lspconfig'
-
-lspconfig.lua_ls.setup{}
-lspconfig.tsserver.setup{}
-lspconfig.tailwindcss.setup{}
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,18 @@
 return {
-  'neovim/nvim-lspconfig'
+  'neovim/nvim-lspconfig',
+  
+  -- Autostart lua_ls server
+  config = function()
+    require('lspconfig').sumneko_lua.setup {
+      cmd = { 'lua-language-server' },
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+        },
+      },
+    }
+  end
+
 }
